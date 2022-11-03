cdf956750ac6b4dc80bdf1a3bb9e878cc9563aeb
r0fld4nc3 <41012911+r0fld4nc3@users.noreply.github.com>
2022-11-03 02:54:16 +0100

    Updated main.py
    - Removed unnecessary code

--- Stellaris Checksum Patcher/main.py
+++ Stellaris Checksum Patcher/main.py
@@ -9,18 +9,6 @@ from stellaris_patcher_menu import entry_menu
 sys.path.append(os.path.join(os.path.dirname(__file__), "Lib\\site-packages"))
 
 # https://steamcommunity.com/sharedfiles/filedetails/?id=2719382752
-
-stellaris_exe_path = r"O:\Steam\steamapps\common\Stellaris\stellaris.exe"
-
-def get_current_dir():
-    # https://stackoverflow.com/a/404750
-
-    if getattr(sys, 'frozen', False):
-        application_path = os.path.dirname(sys.executable)
-    else:
-        application_path = os.path.dirname(__file__)
-
-    return application_path
     
 if __name__ == '__main__':
     patcher = entry_menu.EntryMenu()
