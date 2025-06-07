b6a8d85bc394b8760f3a26211623046faf0f9364
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2025-06-07 23:13:58 +0100

    Merge 'patch-method-tryouts'
    commit 853e94775e858f2387f203ec38a58eb5b60f381f
    Author: Artur <41012911+r0fld4nc3@users.noreply.github.com>
    Date:   Sat Jun 7 23:13:18 2025 +0100
    
        Update log strings
    
        - Better context to what is going on
    
    commit 01ca672e7a18554429e3c74c52021df422a7d9d9
    Author: Artur <41012911+r0fld4nc3@users.noreply.github.com>
    Date:   Fri Jun 6 20:06:20 2025 +0100
    
        Restore previous checksum patch
    
        Adressing some issues with #26
        Determine if "old" pattern is still valid or there is more to look.

--- src/patchers/__init__.py
+++ src/patchers/__init__.py
@@ -1,1 +1,1 @@
-from .stellaris_patch import locate_game_executable, is_patched, create_backup, patch, update_patcher_globals, update_patcher_globals2
+from .stellaris_patch import locate_game_executable, is_patched, create_backup, patch, update_patcher_globals, update_patcher_globals2, update_patcher_globals_old
