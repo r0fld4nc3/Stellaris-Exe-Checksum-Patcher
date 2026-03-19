20f3225976f7b22f11b39724a60a542b0ae6d245
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2026-03-19 04:29:19 +0100

    Update Windows, Linux patterns.
    Update patterns for windows and linux with some added robustness. Will likely need some simplification if possible at some point.

--- src/config/definitions.py
+++ src/config/definitions.py
@@ -7,8 +7,8 @@ HOST: str = "r0fld4nc3"
 APP_FOLDER: str = "Apps"
 APP_NAME: str = "StellarisChecksumPatcher"
 
-REPO_BRANCH: str = "main"
-TRACKING_BRANCH: str = "release"
+REPO_BRANCH: str = "dev"
+TRACKING_BRANCH: str = "dev"
 
 SUPPORTED_GAMES = ("Stellaris",)
 
